cdb98dd7c3bd3f5a6a2defc70b18d165f2ffe903
daniel <danieltrodriguez10@gmail.com>
2018-09-16 19:09:36 -0500

    implementing obstacles

--- src/js/trex.js
+++ src/js/trex.js
@@ -7,7 +7,7 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width;
 var height = canvas.height;
 var modal =  $('#modal');
-
+var nObstacles = 600;
 //  Class
 class Dummy {
     constructor()
@@ -59,10 +59,76 @@ class Dinosaur extends Dummy {
     draw(){
         ctx.drawImage(this.img, this.x, this.y);
     }
+
+
+}
+
+class Obstacles extends Dummy {
+    constructor(x) {
+        super();
+        this.x = x;
+
+        this.hmin = 25;
+        this.hmax = 40;
+
+        this.h = this.generateNumber(this.hmin, this.hmax);
+        console.log(this.h);
+        this.w = this.h * (0.58);
+        this.y = floor - this.h;
+
+        this.min = 1;
+        this.max = 3;
+        this.n = this.generateNumber(this.min, this.max);
+        console.log(this.n);
+
+        this.dmin = 250;
+        this.dmax = 400;
+        this.d = this.generateNumber(this.dmin, this.dmax);
+
+        this.next = null;
+
+        this.img = document.createElement("img");
+        this.img.src = "img/cactus.png";
+    }
+
+    generateNumber(a,b){
+        return Math.floor((Math.random() * b) + a );
+    }
+
+    draw(){
+        var tx = this.x;
+        for (var i = 0; i < this.n; i++) {
+            ctx.drawImage(this.img, tx, this.y, this.w, this.h);
+            tx += this.w;
+        }
+        if (this.next != null) {
+            this.next.draw();
+        }
+    }
+
+    move(){
+
+        this.x -= avance;
+
+        if (this.next != null) {
+            this.next.move();
+        }
+    }
+    add(){
+        if (this.next == null) {
+            this.next = new Obstacles(this.x + this.d);
+        }else{
+            this.next.add();
+        }
+    }
 }
 var world = new World();
 var rex = new Dinosaur();
+var obstacles = new Obstacles(800);
 
+for (i = 0; i < nObstacles; i++) {
+    obstacles.add();
+}
 
 // Functions controller
 var speedJump = 25;
@@ -115,12 +181,14 @@ function draw ()
     ctx.clearRect(0,0, width, height);
     world.draw();
     rex.draw();
+    obstacles.draw();
 }
 
 function frame ()
 {
     draw();
     world.move();
+    obstacles.move();
 }
 
 function start ()
